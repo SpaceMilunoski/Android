1c656788645feaf1df2103491791ac15e4f3f055
SpaseMilunoski <dan.soto576@gmail.com>
2018-04-10 18:49:34 -0500

    ventana ajustes y menu agregados

--- app/src/main/java/com/example/daniel/android/MainActivity.java
+++ app/src/main/java/com/example/daniel/android/MainActivity.java
@@ -80,18 +80,14 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.Ajustes) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.Menu) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.Ayuda) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.Salir) {
+            System.exit(0);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
