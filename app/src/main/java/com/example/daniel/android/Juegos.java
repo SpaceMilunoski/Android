ae977e3379f4e5a4e6a10c3b34ce350bcf16678e
SpaseMilunoski <dan.soto576@gmail.com>
2018-04-23 10:44:03 -0500

    lanzar activiti desde fragmen

--- app/src/main/java/com/example/daniel/android/Juegos.java
+++ app/src/main/java/com/example/daniel/android/Juegos.java
@@ -1,12 +1,14 @@
 package com.example.daniel.android;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.widget.Button;
 
 
 public class Juegos extends Fragment {
@@ -14,8 +16,17 @@ public class Juegos extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.activity_main, container, false);
+        final Button btnCuestionario = (Button) view.findViewById(R.id.btnCuestionario);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_juegos, container, false);
+        btnCuestionario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity() , Ajustes.class );
+                startActivity(i);
+            }
+        });
+        return view;
     }
 
 }
