4f1105763e25e9ccfd211211fac5f800558408de
Miguel Angel <vite_celeste@hotmail.com>
2018-04-22 23:25:44 -0500

    Menu juegos

---
+++ app/src/main/java/com/example/daniel/android/juegos.java
@@ -0,0 +1,19 @@
+package com.example.daniel.android;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class juegos extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_juegos, container, false);
+    }
+}
