a5b62291e5580cd212a3bccf70a0ab73d5d806be
Miguel Angel <vite_celeste@hotmail.com>
2018-04-23 10:31:41 -0500

    Juegos

--- app/src/main/java/com/example/daniel/android/juegos.java
+++ app/src/main/java/com/example/daniel/android/juegos.java
@@ -7,8 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class juegos extends Fragment {
+    public Button btnCuestionario;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
