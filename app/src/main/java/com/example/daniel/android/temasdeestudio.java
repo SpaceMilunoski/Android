94b4c32a84e56e829b9ecece729402f0b5c14275
Tono2007 <bionicle2009@hotmail.es>
2018-04-23 22:33:34 -0500

    introduccion,clases

--- app/src/main/java/com/example/daniel/android/temasdeestudio.java
+++ app/src/main/java/com/example/daniel/android/temasdeestudio.java
@@ -1,49 +1,44 @@
 package com.example.daniel.android;
-import  android.content.Context;
-import android.support.v4.app.Fragment;
+
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-//import android.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-/**
- * Created by Tono2 on 18/04/2018.
- */
-
-public class MenuPrincipal extends Fragment {
+public class temasdeestudio extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-        final View view = inflater.inflate(R.layout.fragment_menuprincipal, container, false);
+        final View view = inflater.inflate(R.layout.temasdeestudio, container, false);
         //final FragmentManager fragmentManager = getFragmentManager();
-        final Button btnJuego = (Button) view.findViewById(R.id.btnJuegos);
-        final Button btnTemas = (Button) view.findViewById(R.id.btnTemasEstudio);
+        final Button btnTema1 = (Button) view.findViewById(R.id.btnIntroduccion);
+        final Button btnTema2 = (Button) view.findViewById(R.id.btnClases);
         //final FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        btnTemas.setOnClickListener(new View.OnClickListener() {
+        btnTema1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //fragmentManager.beginTransaction().replace(R.id.Contenido, new MenuPrincipal()).commit();
                 FragmentTransaction trans = getFragmentManager().beginTransaction();
-                trans.replace(R.id.Contenido, new temasdeestudio()).commit();
+                trans.replace(R.id.Contenido, new Introduccion()).commit();
             }
         });
-        btnJuego.setOnClickListener(new View.OnClickListener() {
+        btnTema2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //fragmentManager.beginTransaction().replace(R.id.Contenido, new MenuPrincipal()).commit();
                 FragmentTransaction trans = getFragmentManager().beginTransaction();
-                trans.replace(R.id.Contenido, new juegos()).commit();
+                trans.replace(R.id.Contenido, new Clases()).commit();
             }
         });
 
         return view;
     }
 }
+
